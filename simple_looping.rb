701be42d2f51e125815e914d2b03e3c11af8bd89
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-03 07:40:53 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -61,6 +61,8 @@ until_iterator(7)
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   for item in number_of_times 
+    puts phrase
+  end
 end
 
 for_iterator(1..7)
